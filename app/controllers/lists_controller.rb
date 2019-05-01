a1576bcbe888ec22ca31616b1a7b8520bcd2c772
github username <github email address>
2019-05-01 13:28:17 -0500

    Built out List Controller Routes and their required code to function

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,30 @@
 class ListsController < ApplicationController
+
+    def index
+        @lists = List.all
+    end
+
+    def new
+        @list = List.new
+    end
+
+    def show
+        @list = List.find_by(params[:id])
+    end
+
+    def create
+        @list = List.new(list_params)
+        if @list.save
+            redirect_to list_path(@list)
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def list_params
+        params.require(:list).permit(:title)
+    end
+
 end
