a667d28d87a961642b32c66b814cc2a9fae371c1
github username <github email address>
2019-05-02 23:07:24 -0500

    Added nested routes for tasks inside of Lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-resources :users
 resources :users do
   resources :lists, only: [:index, :new, :show, :create]
 end
+
+resources :lists do
+  resources :tasks, only: [:index, :new, :create, :show]
+end
+
+
 
 end
