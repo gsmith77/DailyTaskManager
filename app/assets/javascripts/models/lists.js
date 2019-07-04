81a35908c78d769ed15ab935309fe81110559c1f
github username <github email address>
2019-07-04 00:57:42 -0500

    Done.

--- app/assets/javascripts/models/lists.js
+++ app/assets/javascripts/models/lists.js
@@ -1,10 +1,18 @@
 class List {
+    instances = []
     constructor(id, title, user_id){
         this.id = id
         this.title = title
         this.user_id = user_id
+        instances.push(this)
     }
+    indexOfLists(){
+        return console.log(this.instances)
+    }
+    
     totalListCount(listId) {
         return `${listId}` 
     }
+
+
 }
