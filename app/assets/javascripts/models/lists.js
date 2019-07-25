3a550d03351c59bdbb0e91fbe65e907e43a08d9e
Grant Smith <grantsmith1256@gmail.com>
2019-07-24 18:47:09 -0700

    FINISHED :)

--- app/assets/javascripts/models/lists.js
+++ app/assets/javascripts/models/lists.js
@@ -9,17 +9,73 @@ class List {
         .then(function (){
             window.location = `http://localhost:3000/users/${userId}/lists/${listId}.json`
         })
+    };
+
+    newList(list){
+        let date = new Date()
+        const aTag = document.createElement('button')
+        const text = document.createElement('text')
+        text.innerHTML = 'Created on: ' + date
+        aTag.id = "synchronousList"
+        aTag.name = list.id
+        aTag.onclick = List.prototype.showList
+        aTag.innerHTML = list["title"]
+        document.getElementById('current_users_lists').append(text)
+        document.getElementById('current_users_lists').append(aTag)
     }
 
+
+    showList(){        
+        let listId = document.getElementById("synchronousList").name
+        const x = document.getElementById("synchronousList")
+        if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+        }
+        let userId = document.querySelector('hidden_field_tag').id
+        fetch(`http://localhost:3000/users/${userId}/lists/${listId}.json`).then(resp => resp.json())
+        .then((list) => {
+            let aTag = document.createElement('a')
+            aTag.href = `http://localhost:3000/users/${userId}/lists/${list[0]['list']['id']}`
+            aTag.innerHTML = list[0]['list']['title']
+            document.getElementById('current_users_lists').append(aTag)
+            const textTag = document.createElement('text')
+            list.forEach((task) => {
+                textTag.innerHTML += " Task (Content: " + task['content'] + " Status: " + task['status'] + ')'
+            });
+            document.getElementById('current_users_lists').append(textTag)
+        });
+    };
+
     indexOfLists(userId){
-        fetch(`http://localhost:3000/users/${userId}/lists.json`).then(function() {
-            window.location = `http://localhost:3000/users/${userId}/lists.json`
+        fetch(`http://localhost:3000/users/${userId}/lists.json`).then(resp => resp.json()).then((lists) => {   
+        lists.forEach((list) => {
+                let li = document.getElementById('organizedLists1').appendChild(document.createElement('li'));
+      
+                li.innerHTML = list['title']
+            })
         })
-    }
+    };
+
+    indexOfListsWithTasks(userId){
+        fetch(`http://localhost:3000/users/${userId}/lists.json`).then(resp => resp.json()).then((lists) => {   
+        lists.forEach((list) => {
+                let li = document.getElementById('organizedLists2').appendChild(document.createElement('li'));
+                li.style = "color: Blue"
+                li.innerHTML = list['title'] + "  TASKS: "
+                list.tasks.forEach((task) => {
+                    li.innerHTML += " " + task['content'] + " " + "Completed: " + task['status']
+               });
+            })
+        })
+    };
 
     totalListCount(listId) {
         return `${listId}` 
-    }
+    };
 
 
-}
+};
+
+
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,58 +1,59 @@
 class ListsController < ApplicationController
-before_action :authenticate_user
-
-    def index
-        #Index Page in Json
-        if params[:user_id]
-            @user_lists = User.find_by(id: params[:user_id]).lists
-        else
-            @lists = List.all
+    before_action :authenticate_user
+    
+        def index
+            if params[:user_id]
+                @user_lists = User.find_by(id: params[:user_id]).lists
+            else
+                @lists = List.all
+            end
+            
+            respond_to do |f|
+                f.html 
+                f.json {render json: @user_lists || @lists}
+            end
+        end
+    
+        def new
+            @list = List.new
+        end
+    
+        def show
+            @list = List.find(params[:id])
+            @completed_tasks = @list.tasks.completed? 
+            @incomplete_tasks = @list.tasks.incomplete?
+            respond_to do |f|
+                f.html 
+                f.json {render json: @list && @completed_tasks && @incomplete_tasks}
+            end
         end
         
-        respond_to do |f|
-            f.html 
-            f.json {render json: @user_lists || @lists}
+        def create
+            #EMPTY TASK IS CREATED HERE! FIX IT!
+            @list = List.find_or_create_by(list_params)
+            @list.user_id = current_user.id
+            if !current_user.lists.include?(@list) 
+                current_user.lists.push(@list) && @list.save
+                render json: @list, status:201
+            else
+                redirect_to user_path(current_user)
+            end
         end
-    end
-
-    def new
-        @list = List.new
-    end
-
-    def show
-        @list = List.find(params[:id])
-        @completed_tasks = @list.tasks.completed? 
-        @incomplete_tasks = @list.tasks.incomplete?
-        respond_to do |f|
-            f.html 
-            f.json {render json: @list && @completed_tasks && @incomplete_tasks}
+    
+        def destroy
+            if params[:id].present?
+                @list = List.find(params[:id])
+                @list.delete
+                redirect_to user_path(current_user)
+            end
         end
+    
+    
+        private
+    
+        def list_params 
+            params.require(:list).permit(:id, :title, :user_id, tasks_attributes: [:content, :status])
+        end
+    
     end
     
-    def create
-        @list = List.find_or_create_by(list_params)
-        @list.user_id = current_user.id
-        if !current_user.lists.include?(@list) 
-            current_user.lists << @list && @list.save
-            render json: @list, status:201
-        else
-            redirect_to user_path(current_user)
-        end
-    end
-
-    def destroy
-        if params[:id].present?
-            @list = List.find(params[:id])
-            @list.delete
-            redirect_to user_path(current_user)
-        end
-    end
-
-
-    private
-
-    def list_params 
-        params.require(:list).permit(:id, :title, :user_id, tasks_attributes: [:content, :status])
-    end
-
-end
